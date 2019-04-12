39fed61c952c994635cf1403cbf8f08fd5578e2c
Dan Bernstein <danbernstein94@gmail.com>
2019-04-12 13:35:07 -0400

    Add files via upload

--- get_misclassification.R
+++ get_misclassification.R
@@ -10,7 +10,7 @@ col_names <- c('image','obstacle_10%',
                'obstacle_40%','obstacle_45%',
                'obstacle_50%', 'obstacle_centerPoint','obstacle_bikes','label')
 
-raw <- read_csv(commandArgs(trailingOnly = TRUE),
+raw <- read_csv(commandArgs(trailingOnly = TRUE)[1],
                 col_names = col_names)
 
 output <- 
@@ -18,7 +18,8 @@ output <-
   gather(key = 'threshold', value, -label, -image) %>% 
   mutate(value = if_else(value >= 1, 1, 0)) %>% 
   mutate(match = if_else(label == value, 1, 0)) %>% 
-  filter(match == 0)
+  filter(match == 0) %>% 
+  slice(commandArgs(trailingOnly = TRUE)[2])
 
 print(output)
 
