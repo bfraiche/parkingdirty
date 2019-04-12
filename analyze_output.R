97ccc087f42ea6bb2ded636759dc3b34f575577e
Dan Bernstein <danbernstein94@gmail.com>
2019-04-12 13:10:15 -0400

    R script to analyze threshold values
    this script processes the output from process_images to determine the optimal threshold for overlap between lanes and objects to classify an object as an obstacle

---
+++ analyze_output.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(readr)
+library(stringr)
+library(ggplot2)
+library(tidyr)
+
+col_names <- c('image','obstacle_10%',
+               'obstacle_15%','obstacle_20%','obstacle_25%',
+               'obstacle_30%','obstacle_35%',
+               'obstacle_40%','obstacle_45%',
+               'obstacle_50%', 'obstacle_centerPoint','obstacle_bikes','label')
+
+raw <- read_csv(commandArgs(trailingOnly = TRUE),
+                col_names = col_names)
+
+output <- 
+  raw %>%
+  gather(key = 'threshold', value, -label, -image) %>% 
+  mutate(value = if_else(value >= 1, 1, 0)) %>% 
+  mutate(match = if_else(label == value, 1, 0)) %>% 
+  group_by(threshold) %>% 
+  summarize(tot_correct = sum(match),
+            perc_correct = tot_correct / n()) %>% 
+  arrange(desc(tot_correct))
+
+print(output)
