c41e06669b038308ac17f611a0d10a3708187a32
Dan Bernstein <danbernstein94@gmail.com>
2019-04-15 12:02:40 -0400

    Add files via upload

--- get_misclassification.R
+++ get_misclassification.R
@@ -4,28 +4,24 @@ library(stringr)
 library(ggplot2)
 library(tidyr)
 
-get_misclassification <- function(){
-  col_names <- c('image','obstacle_10%',
-                 'obstacle_15%','obstacle_20%','obstacle_25%',
-                 'obstacle_30%','obstacle_35%',
-                 'obstacle_40%','obstacle_45%',
-                 'obstacle_50%', 'obstacle_centerPoint','obstacle_bikes','label')
-  
-  args <- commandArgs(trailingOnly = TRUE)
-  
-  raw <- read_csv(args[1],
-                  col_names = col_names)
-  
-  output <- 
-    raw %>%
-    gather(key = 'threshold', value, -label, -image) %>% 
-    mutate(value = if_else(value >= 1, 1, 0)) %>% 
-    mutate(match = if_else(label == value, 1, 0)) %>% 
-    filter(match == 0) %>% 
-    slice(1:args[2])
-  
-  print(output)
-  
-  return(output)
-  }
+col_names <- c('image','obstacle_10%',
+               'obstacle_15%','obstacle_20%','obstacle_25%',
+               'obstacle_30%','obstacle_35%',
+               'obstacle_40%','obstacle_45%',
+               'obstacle_50%', 'obstacle_centerPoint','obstacle_bikes','label')
 
+args <- commandArgs(trailingOnly = TRUE)
+
+raw <- read_csv(args[1],
+                col_names = col_names)
+
+output <- 
+  raw %>%
+  gather(key = 'threshold', value, -label, -image) %>% 
+  mutate(value = if_else(value >= 1, 1, 0)) %>% 
+  mutate(match = if_else(label == value, 1, 0)) %>% 
+  filter(match == 0) %>% 
+  slice(1:args[2])
+
+print(output)
+  
